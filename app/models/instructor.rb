dc4d658e7c370e7ec78b72f27c1bd9cb5933e806
Brian <bbensch@gmail.com>
2016-09-19 11:41:20 -0700

    update prices to 5/hr base rate, enable admin notifications for new lesson signup

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,8 +1,17 @@
 class Instructor < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :locations
+  after_create :send_admin_notification
+
 
   def name
     self.first_name + " " + self.last_name
   end
+
+  def send_admin_notification
+      @user = Instructor.last.user
+      LessonMailer.new_instructor_application_received(@user).deliver
+      puts "an admin notification has been sent."
+  end
+
 end
