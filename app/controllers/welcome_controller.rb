627e7d3717cea71b9fdf6a84b7ab920b3ca1bcdb
Brian <bbensch@gmail.com>
2016-10-06 23:29:21 -0700

    pause email admin notifications for Apply pageviews

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,4 @@
+
 class WelcomeController < ApplicationController
     skip_before_action :authenticate_user!
   def index
@@ -12,11 +13,11 @@ class WelcomeController < ApplicationController
     puts "The GA tracking code is known to be: #{ga_test_code} and the endpoint is #{ga_endpoint}."
     GoogleAnalyticsApi.new.event('instructor-recruitment', 'load-application-page')
     puts "-------attempt to fire new event for loading application page"
-    if current_user.nil?
-        LessonMailer.track_apply_visits.deliver
-      else
-        # LessonMailer.track_apply_visits(current_user.email).deliver
-    end
+    # if current_user.nil?
+    #     LessonMailer.track_apply_visits.deliver
+    #   else
+    #     LessonMailer.track_apply_visits(current_user.email).deliver
+    # end
   end
 
   def notify_admin
