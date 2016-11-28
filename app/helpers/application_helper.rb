c97994990b37c6bbd219d921c2cda4af70dd976e
Brian <bbensch@gmail.com>
2016-11-28 01:18:54 -0800

    update meta tags for titles & descriptiosn on core landing pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,11 +22,19 @@ end
 
 def title(title = nil)
     if title.present?
-      content_for :title, title
+      content_for :title, title + ' | Snow Schoolers '
     else
       content_for?(:title) ? content_for(:title) : 'Snow Schoolers'
     end
 end
+
+  def meta_tag(tag, text)
+    content_for :"meta_#{tag}", text
+  end
+
+  def yield_meta_tag(tag, default_text='Browse instructors, compare prices, and book lessons at your favorite resort.')
+    content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
+  end
 
 
 end
