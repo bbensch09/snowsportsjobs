c9a8ad51e4807c47fe20af99322d94cdd4beda30
Brian <bbensch@gmail.com>
2016-12-01 22:35:15 -0800

    add favicon logo to stripe checkout; update actionmailer host to be based on ENV variable for domain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   #Suppress logger output for asset requests
   config.assets.quiet = true
   #GMAIL CONFIG
-  config.action_mailer.default_url_options = { :host => 'snowschoolers.com' }
+  config.action_mailer.default_url_options = { :host => ENV['HOST_DOMAIN'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
    #config.action_mailer.perform_deliveries = false
