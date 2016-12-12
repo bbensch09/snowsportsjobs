14b1c9330af7e65a6e971289554eacfcd96f6a67
Brian <bbensch@gmail.com>
2016-12-11 16:01:49 -0800

    restore commented option to allow next day lessons only

--- app/assets/javascripts/lessons.js
+++ app/assets/javascripts/lessons.js
@@ -33,6 +33,7 @@ var LESSON = {
   },
 
   setDatepicker: function() { LESSON._date.datepicker({ minDate: '2016-12-16', dateFormat: 'yy-mm-dd' }); },
+  // setDatepicker: function() { LESSON._date.datepicker({ minDate: 1, dateFormat: 'yy-mm-dd' }); },
 
   // var TODAYS_DATE = new Date().getDate();
   // var LAUNCH_DATE = new Date('2016','11','16').getDate();
