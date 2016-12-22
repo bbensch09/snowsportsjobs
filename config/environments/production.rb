658989211cf3f6f1cfb405bf8a88863aa6c44e89
Brian <bbensch@gmail.com>
2016-12-21 23:54:01 -0800

    force rails to use SSL in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,10 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  #force SSL on all pages
+  config.force_ssl = true
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
