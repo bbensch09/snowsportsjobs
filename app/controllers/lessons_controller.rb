2c46c770677c3d5ab4eaf98ca00ef631b91b73e9
Brian <bbensch@gmail.com>
2017-01-04 08:29:36 -0700

    fix duplicate guest email error, lookup by name rather than email, which defualts to downcase

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -142,16 +142,16 @@ class LessonsController < ApplicationController
     @lesson.lesson_time = @lesson_time = LessonTime.find_or_create_by(lesson_time_params)
     @lesson.requester = current_user
     if @lesson.guest_email && @lesson.requester.nil?
-      guest_user_params = {
+      if User.find_by_name("bbensch+testJan3@gmail.com")
+          @lesson.requester_id = User.find_by_name("bbensch+testJan3@gmail.com").id
+      else
+          User.create!({
           email: @lesson.guest_email,
           password: 'homewood_temp_2016',
           user_type: "Student",
           name: "#{@lesson.guest_email}"
-      }
-      if User.create(guest_user_params)
-          @lesson.requester_id = User.last.id
-      else
-         @lesson.requester_id = User.find_by_email(@leson.guest_email).id
+          })
+         @lesson.requester_id = User.last.id
       end
     end
     unless @lesson.deposit_status == 'confirmed'
