e7d0f813d7f6ff755efbba653777d8ca79979f83
Brian <bbensch@gmail.com>
2017-02-07 02:22:54 -0800

    sort isntructors by review count #2

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -41,7 +41,7 @@ class InstructorsController < ApplicationController
     puts "Session values stored: #{session[:lesson]}"
     @instructors = Instructor.where(status: "Active")
     @instructors = @instructors.to_a.keep_if {|instructor| instructor.ski_levels.any? || instructor.snowboard_levels.any? }
-    @instructors.sort {|a,b| b.reviews.count <=> a.reviews.count}
+    @instructors.sort! {|a,b| b.reviews.count <=> a.reviews.count}
   end
 
   # GET /instructors/1
