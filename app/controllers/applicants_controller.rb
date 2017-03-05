bbc4d45c78473302db8b27c78b8150118f8d7f1b
Brian <bbensch@gmail.com>
2017-03-05 09:39:03 -0800

    update redirect post application

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -34,8 +34,8 @@ class ApplicantsController < ApplicationController
 
     respond_to do |format|
       if @applicant.save
-        GoogleAnalyticsApi.new.event('instructor-recruitment', 'new-application-submitted', params[:ga_client_id])
-        format.html { render 'instructors/thank_you', notice: 'Your instructor application was successfully submitted, you will be contacted shortly. You may also reach out with questions to info@snowschoolers.com' }
+        GoogleAnalyticsApi.new.event('liftops-recruitment', 'new-application-submitted', params[:ga_client_id])
+        format.html { redirect_to root_path, notice: 'Your application was successfully submitted, you will be contacted shortly. You may also reach out with questions to info@snowschoolers.com' }
         format.json { render action: 'show', status: :created, location: @applicant }
       else
         format.html { render :new }
