864b9da75be5c61cdc77cb354e5ac8a9899a0bd7
Brian <bbensch@gmail.com>
2017-04-20 11:47:48 -0700

    add closing date to locations, update forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412231354) do
+ActiveRecord::Schema.define(version: 20170420182826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -237,6 +237,7 @@ ActiveRecord::Schema.define(version: 20170412231354) do
     t.boolean  "night_skiing"
     t.string   "city"
     t.string   "state_abbreviation"
+    t.date     "closing_date"
   end
 
   create_table "messages", force: :cascade do |t|
